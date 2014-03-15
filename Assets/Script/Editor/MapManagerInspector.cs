a0a7d5e98796e8045a8758808cb23f708297a8bd
windroni <takeaya@daum.net>
2014-03-15 15:59:58 +0900

    저장 로드 까지..

--- Assets/Script/Editor/MapManagerInspector.cs
+++ Assets/Script/Editor/MapManagerInspector.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using System.IO;
 
 /*
  * 텍스처의 이해 (파이프라인에 대한 이해)
@@ -46,10 +47,9 @@ public class MapManagerInspector : Editor
 	public override void OnInspectorGUI()
 	{
 		this.DrawDefaultInspector ();
-		//CommonEditorUi.DrawSeparator ();
-		//DrawMinMaxValues ();
-		//DrawBaseTileObject ();
+		DrawPathData();
 		DrawGenerateButton();
+		DrawSaveLoadButton();
 	}
 
 
@@ -70,11 +70,8 @@ public class MapManagerInspector : Editor
 	}
 
 
-	void OnSceneGUI()
+	void DrawUI()
 	{
-		if(Application.isPlaying)
-			return;
-
 		Handles.BeginGUI();
 		if(GUI.Button(new Rect(10, 10, 100, 30), "NORMAL"))
 		{
@@ -96,76 +93,238 @@ public class MapManagerInspector : Editor
 		{
 			mapManager_._editTileStyle = TILESTYLE.END;
 		}
-
+		
 		GUI.color = Color.green;
 		GUI.Label(new Rect(120, 10, 500, 30), "Eidt Mode : " + mapManager_._editTileStyle);
 		GUI.color = Color.white;
-
+		
 		Handles.EndGUI();
+	}
 
+	void ProcessHotKey(char value)
+	{
+		if(value == '1') mapManager_._editTileStyle = TILESTYLE.NORMAL;
+		else if(value == '2') mapManager_._editTileStyle = TILESTYLE.STRAIGHT;
+		else if(value == '3') mapManager_._editTileStyle = TILESTYLE.CORNER;
+		else if(value == '4') mapManager_._editTileStyle = TILESTYLE.START;
+		else if(value == '5') mapManager_._editTileStyle = TILESTYLE.END;
+	}
+
+	void OnSceneGUI()
+	{
+		if(Application.isPlaying)
+			return;
+		
+		DrawUI();
+		
 		int controlID = GUIUtility.GetControlID(FocusType.Passive);
 		HandleUtility.AddDefaultControl(controlID);
-
+		
 		Event e = Event.current;
 		if(e.isKey)
 		{
-			Debug.Log("key click");
-			if(e.character == '1') mapManager_._editTileStyle = TILESTYLE.NORMAL;
-			else if(e.character == '2') mapManager_._editTileStyle = TILESTYLE.STRAIGHT;
-			else if(e.character == '3') mapManager_._editTileStyle = TILESTYLE.CORNER;
-			else if(e.character == '4') mapManager_._editTileStyle = TILESTYLE.START;
-			else if(e.character == '5') mapManager_._editTileStyle = TILESTYLE.END;
-		}
-
+			ProcessHotKey(e.character);
+		}	
 		else if(e.type == EventType.mouseDown || e.type == EventType.mouseDrag)
 		{
 			if(e.alt)
 			{
 				return;
 			}
-
+			
+			// 현재 마우스 버튼의 좌료를 얻는다.(2D)
 			Vector2 mousePosition = Event.current.mousePosition;
-
-			Ray ray = HandleUtility.GUIPointToWorldRay(mousePosition);
+			Ray ray = HandleUtility.GUIPointToWorldRay(mousePosition); // 카메라의 점과 마우스가 찍은 점을 연결시켜 레이를 만든다.
+			
 			RaycastHit hit;
-			bool result  = Physics.Raycast(ray, out hit, 1000.0f);
-
+			bool result  = Physics.Raycast(ray, out hit, 1000.0f); //레이가 지나가면서 충돌된 물체는 hit에 담긴다.
+			
+			GameObject tileObj = null;
+			TileInfo tileInfo = null;
+			
 			if(result)
 			{
-				GameObject tileObj = hit.transform.gameObject;
-				TileInfo tileInfo = tileObj.GetComponent<TileInfo>();
+				tileObj = hit.transform.gameObject;
+				tileInfo = tileObj.GetComponent<TileInfo>();
+				
 				if(tileInfo == null)
 					return;
+				
 				if(e.button == 0) //left click
 				{
-					Debug.Log("edit left click");
-
-					tileInfo.currnetTileStyle = mapManager_._editTileStyle;
-					tileInfo.UpdateMaterial();
+					if(e.shift)
+					{
+						if(mapManager_._pathList.Contains(hit.transform) == false)
+						{
+							mapManager_._pathList.Add(hit.transform);
+							tileInfo.currnetTileStyle = mapManager_._editTileStyle;
+							tileInfo.UpdateMaterial();
+						}
+					}
+					else
+					{
+						tileInfo.currnetTileStyle = mapManager_._editTileStyle;
+						tileInfo.UpdateMaterial();
+					}
 				}
 				else if(e.button == 1) //right click
 				{
-					hit.transform.localEulerAngles += new Vector3(0.0f, 90.0f, 0.0f);
+					if(e.shift)
+					{
+						if(mapManager_._pathList.Contains(hit.transform) == true)
+						{
+							mapManager_._pathList.Remove(hit.transform);
+						}
+					}
+					else
+					{
+						hit.transform.localEulerAngles += new Vector3(0.0f, 90.0f, 0.0f);
+					}
 				}
+
+
 			}
+		}
+		
+		DrawPathList();
+	}
+
+
+
+	void DrawPathList()
+	{
+		for(int i = 0; i < mapManager_._pathList.Count; ++i)
+		{
+			Transform obj = mapManager_._pathList[i];
+			TileInfo tileInfo = obj.GetComponent<TileInfo>();
+			if(tileInfo.currnetTileStyle == TILESTYLE.START)
+			{
+				Handles.color = Color.green;
+			}
+			else if(tileInfo.currnetTileStyle == TILESTYLE.END)
+			{
+				Handles.color = Color.red;
+			}
+
+
+			Handles.DrawSphere(i, obj.position + Vector3.up * 0.5f,
+			                   obj.rotation, 0.5f);
+			
+			Handles.color = Color.white;
+		}
+
+		if(mapManager_._pathList.Count > 2)
+		{
+			Handles.color = Color.magenta;
+			for(int i= 0; i < mapManager_._pathList.Count - 1; ++i)
+			{
+				Transform obj1 = mapManager_._pathList[i];
+				Transform obj2 = mapManager_._pathList[i + 1];
+				Vector3 pos1 = obj1.position + Vector3.up * 0.5f;
+				Vector3 pos2 = obj2.position + Vector3.up * 0.5f;
+				Handles.DrawLine(pos1, pos2);
+			}
+
+			Handles.color = Color.white;
 		}
 	}
 
-	void PathAddOrRemove(int buttonCode, Event currentEvent, RaycastHit hit, TileInfo tileInfo)
+	void DrawPathData()
 	{
-		if(buttonCode == 0)
+		if(GUILayout.Button("Remove All Path Data"))
 		{
-			if(currentEvent.shift)
-			{
-				if(mapManager_._pathList.Contains(hit.transform) == false)
-					mapManager_._pathList.Add(hit.transform);
-			}
-			else
-			{
-				tileInfo
-			}
+			mapManager_._pathList.Clear();
+		}
+
+		string pathLabel = "Path List : "  + mapManager_._pathList.Count;
+		EditorGUILayout.LabelField(pathLabel);
+
+		for(int i = 0; i < mapManager_._pathList.Count; ++i)
+		{
+			string pathName = "Path Index : " + i;
+			Transform pathTransform = mapManager_._pathList[i];
+			EditorGUILayout.ObjectField(pathName, pathTransform, typeof(GameObject));
 		}
 	}
+
+
+
+	public void DrawSaveLoadButton()
+	{
+		if(string.IsNullOrEmpty(mapManager_._fileName))
+			mapManager_._fileName = "Map";
+
+		string fileName = EditorGUILayout.TextField("Map File Name", mapManager_._fileName);
+
+		if(fileName != mapManager_._fileName)
+		{
+			CommonEditorUi.RegisterUndo("Map File Name", mapManager_);
+			mapManager_._fileName = fileName;
+		}
+
+		EditorGUILayout.BeginHorizontal();
+		if(GUILayout.Button("Save Map Data"))
+		{
+			SaveFile();
+		}
+
+		if(GUILayout.Button("Laod Map Data"))
+		{
+			mapManager_.LoadFile();
+		}
+
+		EditorGUILayout.EndHorizontal();
+	}
+
+	void SaveFile()
+	{
+		string dataPath = Application.dataPath;
+		string fullPath = dataPath + "/Resources/MapData/" + mapManager_._fileName + ".txt";
+
+		FileStream fs = new FileStream(fullPath, FileMode.Create);
+		TextWriter textWriter = new StreamWriter(fs);
+
+		int width = mapManager_.currentMapWidth_;
+		int height = mapManager_.currentMapHeigth_;
+
+		textWriter.Write("width " + width + "\n");
+		textWriter.Write("height " + height + "\n");
+
+		for(int i = 0; i < width; ++i)
+		{
+			for(int j = 0; j < height; ++j)
+			{
+				Transform tile = mapManager_.tiles_[i,j].transform;
+				textWriter.Write(tile.position + "\t");
+				textWriter.Write(tile.eulerAngles + "\t");
+
+				TileInfo tileInfo = tile.GetComponent<TileInfo>();
+				textWriter.Write((int)tileInfo.currnetTileStyle + "\t");
+				textWriter.Write("\n");
+			}
+		}
+
+
+		//save Path
+		int pathCount = mapManager_._pathList.Count;
+		textWriter.Write("PathCount " + pathCount + "\n");
+
+		for(int i = 0; i < pathCount; ++i)
+		{
+			string [] tileIndex = mapManager_._pathList[i].name.Split('_');
+			textWriter.Write(tileIndex[0] + "\t");
+			textWriter.Write(tileIndex[1]);
+			textWriter.Write("\n");
+		}
+
+		textWriter.Close();
+	}
+
+
+
+
+
+
 
 
 
@@ -173,7 +332,7 @@ public class MapManagerInspector : Editor
 
 	void DrawBaseTileObject()
 	{
-		GUI.color = (mapManager_._baseTilePrefab != null) ? Color.green : Color.red;
+		/*GUI.color = (mapManager_._baseTilePrefab != null) ? Color.green : Color.red;
 
 		GameObject baseTilePrefab = (GameObject)EditorGUILayout.ObjectField ("Base Field", mapManager_._baseTilePrefab, typeof(GameObject));
 		if (baseTilePrefab != mapManager_._baseTilePrefab) 
@@ -182,7 +341,7 @@ public class MapManagerInspector : Editor
 			mapManager_._baseTilePrefab = baseTilePrefab;
 		}
 
-		GUI.color = Color.white;
+		GUI.color = Color.white;*/
 	}
 
 	void DrawMinMaxValues()
--- Assets/Script/MapManager.cs
+++ Assets/Script/MapManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
 // 데이타에서 값을 읽어 범위를 지정하게 할수는 없나?
 public class MapManager : MonoBehaviour 
@@ -23,7 +24,10 @@ public class MapManager : MonoBehaviour
 	public GameObject _baseTilePrefab;
 
 	public TILESTYLE _editTileStyle = TILESTYLE.NORMAL;
-	
+	public List<Transform> _pathList = new List<Transform>();
+
+	public string _fileName;
+
 
 	public void RemoveAllTiles()
 	{
@@ -41,8 +45,89 @@ public class MapManager : MonoBehaviour
 		gameObjectList.Clear();
 		gameObjectList = null;
 		tiles_ = null;
+		
+		_pathList.Clear();
+	}
 
-		//pathList_.Clear();
+	public void LoadFile()
+	{
+		RemoveAllTiles();
+
+		string dataPath = Application.dataPath;
+		//string filePath = dataPath + "/Resources/MapData/" + _fileName;
+		
+		string filePath = "MapData/" + _fileName;
+		TextAsset asset = (TextAsset)Resources.Load(filePath, typeof(TextAsset));
+		
+		if(asset == null)
+		{
+			Debug.Log("Can not open file on Asset/" + filePath + ".txt");
+			return;
+		}
+		
+		Stream stream = new MemoryStream(asset.bytes);
+		TextReader textReader  = new StreamReader(stream);
+		
+		// read.width	
+		string text = textReader.ReadLine();
+		int spaceIndex = text.IndexOf(' ');
+		string widthText = text.Substring(spaceIndex + 1);
+		Debug.Log(widthText);
+		currentMapWidth_ = int.Parse(widthText);
+
+		// read.height	
+		text = textReader.ReadLine();
+		string heightText = text.Substring(text.IndexOf(' ') + 1);
+		currentMapHeigth_ = int.Parse(heightText);
+
+		// Create Map
+		tiles_ = new GameObject[currentMapWidth_, currentMapHeigth_];
+		for(int i = 0; i < currentMapWidth_; ++i)
+		{
+			for(int j = 0; j < currentMapHeigth_; ++j)
+			{
+				text = textReader.ReadLine();
+				string[] infos = text.Split('\t');
+				GameObject obj = Instantiate(_baseTilePrefab) as GameObject;
+				obj.name = i + "_" + j;
+				obj.transform.parent = transform;
+				obj.transform.localPosition = GetVector3FromString(infos[0]);
+				obj.transform.eulerAngles = GetVector3FromString(infos[1]);
+
+				TileInfo tileInfo = obj.GetComponent<TileInfo>();
+				tileInfo.currnetTileStyle = (TILESTYLE)(int.Parse(infos[2]));
+				tileInfo.UpdateMaterial();
+				tiles_[i,j] = obj;
+			}
+		}
+
+		// load path 
+		text = textReader.ReadLine();
+		string pathCountText = text.Substring(text.IndexOf(' '));
+		int pathCount = int.Parse(pathCountText);
+		for (int i = 0; i < pathCount; ++i)
+		{
+			text = textReader.ReadLine();
+			
+			string[] tiles = text.Split('\t');
+			int x = int.Parse(tiles[0]);
+			int y = int.Parse(tiles[1]);
+			
+			_pathList.Add(tiles_[x, y].transform);
+		}
+	}
+
+	public Vector3 GetVector3FromString(string text)
+	{
+		string newText = text.Replace('(', ' ');
+		newText = newText.Replace(')', ' ');
+		
+		string[] datas = newText.Split(',');
+		
+		float x = float.Parse(datas[0]);
+		float y = float.Parse(datas[1]);
+		float z = float.Parse(datas[2]);
+		return new Vector3(x, y, z);
 	}
 
 #if UNITY_EDITOR
